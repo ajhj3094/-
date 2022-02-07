2e844177eab7dcadf4217243d1aba00d6c233208
Justion Yu <ajhj80529@gmail.com>
2022-02-07 16:31:48 +0800

    Logo & text-field

--- hiver/src/App.vue
+++ hiver/src/App.vue
@@ -18,6 +18,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import './assets/scss/main.scss';
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
 </style>
--- hiver/src/components/Navbar.vue
+++ hiver/src/components/Navbar.vue
@@ -1,13 +1,27 @@
 <template lang="pug">
-v-app-bar#navbar(app prominent)
+v-app-bar#navbar(
+    app
+    prominent
+  )
   .w-100.sd-color
     v-container.xl-mw.navtext-container.pa-0
       p.mb-0.text-subtitle-2.white--text
         | 超取$199 免運費，宅配$499 免運費(大型商品除外)。滿千送百活動獲得的購物抵用卡開始使用囉！
   .w-100
-    v-container.xl-mw.header-container.pa-0
-      .logo
-      v-text-field
+    v-container.xl-mw.header-container.pa-0.d-flex
+      v-img.mr-8(
+        :src="require('../assets/logo.png')"
+        contain
+        max-width='125'
+      )
+      v-text-field.flex-grow-0.align-center(
+        clearable='true'
+        dense
+        @click:append='text++'
+      )
+        template(#append)
+          v-btn(icon)
+            v-icon mdi-magnify
 </template>
 
 <script>
